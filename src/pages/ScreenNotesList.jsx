5616ebef3fa240ad1898991b9b25689ac423b9c5
Raphael Freitas Nogueira de Souza <raphaelfreitas18.dev@gmail.com>
2023-02-16 16:23:02 -0300

    first commit

---
+++ src/pages/ScreenNotesList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export function ScreenNotesList() {
+  return (
+    <View style={styles.container}>
+      <Text>Notes Page</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  }
+})
